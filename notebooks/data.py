eb59d01e451837fb4638d43aa550c2b261963703
Tingkai Liu <tl2747@columbia.edu>
2022-02-28 16:40:23 -0500

    add notebooks

---
+++ notebooks/data.py
@@ -0,0 +1,285 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Contrast and Concentration Invariance
+# This notebook illustrates the following:
+# 1. Decomposition of OSN and PN responses into steady-state and transient components,
+# 2. Computing Odorant Concentration Contrast
+# 3. Measuring concentration invariance and contrast boosting by comparing steady-state and transient
+#    OSN and PN responses to concentration waveform and concentration contrast.
+#
+# Python version `>=3.8` is used for this notebook with the following required packages:
+# ```
+# numpy>=1.20
+# matplotlib>=3.4
+# seaborn>=0.11
+# scipy>=1.8
+# tqdm>=4.60
+# ```
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, find_peaks
+import seaborn as sns
+from tqdm.auto import tqdm
+
+# %%
+data = np.load('../data/staircase.npz', allow_pickle=True)
+t = data['t']
+dt = t[1] - t[0]
+
+# %% [markdown]
+# ## Plot raw data
+
+# %%
+fig,axes = plt.subplots(
+    4, 7, figsize=(20,8), 
+    sharex=True, sharey='row', 
+    gridspec_kw=dict(hspace=.5, wspace=.3)
+)
+
+for n, (osn_i, osn_o, pn_i, pn_o) in enumerate(zip(
+    data['osn_input'],
+    data['osn_output'],
+    data['pn_input'],
+    data['pn_output'],
+)):
+    axes[0,n].plot(t, osn_i, c='k')
+    axes[0,n].set(title='Acetone Concentration')
+    axes[1,n].plot(t, osn_o, c='k')
+    axes[1,n].set(title='Or59b OSN Response')
+    axes[2,n].plot(t, pn_i, c='k')
+    axes[2,n].set(title='Acetone Concentration')
+    axes[3,n].plot(t, pn_o, c='k')
+    axes[3,n].set(title='DM4 PN Response')
+_ = [ax.set(xlabel='Time [sec]') for ax in axes[-1]]
+axes[0,0].set(ylabel='Concentration [ppm]')
+axes[1,0].set(ylabel='PSTH [Hz]')
+axes[2,0].set(ylabel='Concentration [ppm]')
+axes[3,0].set(ylabel='PSTH [Hz]')
+
+
+# %% [markdown]
+# ## Decompose PN responses to get transient and steady-state components
+
+# %%
+def get_contrast(t, stim, contrast_eps=1, axis=-1):
+    dt = t[1] - t[0]
+    b, a = butter(5, 15, fs=1 / dt)
+    stim_smooth = lfilter(b, a, np.clip(stim, 0, np.inf), axis=axis)
+    d_stim = np.diff(stim_smooth, axis=axis, prepend=stim_smooth[:, [0]]) / dt
+    contrast_stim = d_stim / (contrast_eps + stim_smooth)
+    return contrast_stim
+
+
+# %%
+def decompose_signal(
+    t, stim, out, t_bounds=None, axis=-1, contrast_eps=1.0, ss_window=0.5
+):
+    dt = t[1] - t[0]
+    # compute contrast
+    contrast_stim = get_contrast(t, stim, axis=axis)
+    # find timing of peak contrasts (both positive and negative)
+    pos_peaks_contrast = np.sort(
+        np.concatenate(
+            [
+                find_peaks(
+                    contrast_stim[0], height=2.5, width=100, distance=int(1.7 // dt)
+                )[0],
+                find_peaks(
+                    -contrast_stim[0], height=2.5, width=100, distance=int(1.7 // dt)
+                )[0],
+            ]
+        )
+    )
+    pos_peaks_contrast = np.concatenate([pos_peaks_contrast, [len(t)]])
+    
+    # find average response ss_window seconds before each contrast peak
+    ss = np.zeros_like(out)
+    ss_vals = np.zeros((out.shape[0], len(pos_peaks_contrast) - 1))
+    for n, (start_idx, stop_idx) in enumerate(
+        zip(pos_peaks_contrast[:-1], pos_peaks_contrast[1:])
+    ):
+        ss_amp = out[
+            :,
+            stop_idx - int((0.2 + ss_window) // (dt)) : 
+            stop_idx - int(0.2 // (dt)),
+        ].mean(axis=1)
+        ss_vals[:, n] = ss_amp
+        ss[:, start_idx:stop_idx] = ss_amp[:, None]
+    
+    # compute peak as residual
+    pk = out - ss
+    return pos_peaks_contrast, ss_vals, ss, pk
+
+
+# %%
+def yyaxis(ax: plt.Axes, c: "color" = "red") -> plt.Axes:
+    """Create A second axis with colored spine/ticks/label
+
+    Note:
+        This method will only make the twinx look like the color in
+        MATLAB's :code:`yyaxis` function. However, unlike in MATLAB,
+        it will not set the linestyle and linecolor of the lines that
+        are plotted after twinx creation.
+
+    Arguments:
+        ax: the main axis to generate a twinx from
+        c: color of the twinx, see https://matplotlib.org/stable/gallery/color/color_demo.html
+            for color specifications accepted by matplotlib.
+    """
+    ax2 = ax.twinx()
+    ax2.spines["right"].set_color(c)
+    ax2.tick_params(axis="y", colors=c, which="both")
+    ax2.yaxis.label.set_color(c)
+    return ax2
+
+
+# %%
+fig,axes = plt.subplots(
+    4, 7, figsize=(20,8), 
+    sharex=True, sharey='row', 
+    gridspec_kw=dict(hspace=.5, wspace=.5)
+)
+
+for n, (pn_i, pn_o) in enumerate(zip(
+    data['pn_input'],
+    data['pn_output'],
+)):
+    peak_time, _, steady_state, peak = decompose_signal(t, pn_i[None,:], pn_o[None,:])
+    u_contrast = get_contrast(t, pn_i[None,:])
+    axes[0,n].plot(t, pn_i.T, c='k')
+    axes[0,n].set(title='Acetone Concentration')
+    axes[1,n].plot(t, pn_o.T, c='k')
+    axes[1,n].set(title='DM4 PN Response')
+    axes[2,n].plot(t, steady_state.T, c='k')
+    axes[2,n].set(title='Steady-State Response')
+    axes[3,n].plot(t, peak.T, c='k')
+    axes[3,n].set(ylim=[-100, 200])
+    ax2 = yyaxis(axes[3,n], c='r')
+    ax2.plot(t, u_contrast.T, c='r')
+    if n == 0:
+        ax2.set(ylabel='Contrast [/s]')
+    ax2.set(ylim=[-10, 20])
+    axes[3,n].set(title='Transient Response \nvs. Contrast')
+_ = [ax.set(xlabel='Time [sec]') for ax in axes[-1]]
+axes[0,0].set(ylabel='Concentration [ppm]')
+axes[1,0].set(ylabel='PSTH [Hz]')
+axes[2,0].set(ylabel='PSTH [Hz]')
+axes[3,0].set(ylabel='PSTH [Hz]')
+
+
+# %% [markdown]
+# ## Correlation Steady-State vs. Concentration Waveform, Transient vs. Contrast
+
+# %%
+def corrcoef(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """Return Correlation Coefficient of two matricces"""
+    mu_x = x.mean(1)
+    mu_y = y.mean(1)
+    x_center = x - mu_x[:, None]
+    y_center = y - mu_y[:, None]
+    num = (x_center * y_center).sum(1)
+    den = np.linalg.norm(x_center, axis=1) * np.linalg.norm(y_center, axis=1)
+    return num / (1e-15 + den)
+
+
+
+# %%
+delays = np.arange(int(-.1//dt), int(.1//dt), 10)
+corr = dict(xy = [], corr = [], neuron=[])
+
+for n, (inp, out) in tqdm(
+    enumerate(zip(data['osn_input'], data['osn_output'])), 
+    total=len(data['pn_input'])
+):
+    _, _, ss, pk = decompose_signal(t, inp[None,:], out[None,:])
+    ss = np.squeeze(ss)
+    pk = np.squeeze(pk)
+    inp_contrast = np.squeeze(get_contrast(t, inp[None,:]))
+    output_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(out, d) for d in delays]),
+        inp[None,:]
+    ))
+    ss_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(ss, d) for d in delays]),
+        inp[None,:]
+    ))
+    pk_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(pk, d) for d in delays]),
+        inp_contrast[None,:]
+    ))
+    
+    corr['xy'].append('response vs. concentration')
+    corr['corr'].append(output_conc_corr)
+    corr['neuron'].append('osn')
+    
+    corr['xy'].append('steady-state vs. concentration')
+    corr['corr'].append(ss_conc_corr)
+    corr['neuron'].append('osn')
+    
+    corr['xy'].append('transient vs. contrast')
+    corr['corr'].append(pk_conc_corr)
+    corr['neuron'].append('osn')
+
+for n, (inp, out) in tqdm(
+    enumerate(zip(data['pn_input'], data['pn_output'])), 
+    total=len(data['pn_input'])
+):
+    _, _, ss, pk = decompose_signal(t, inp[None,:], out[None,:])
+    ss = np.squeeze(ss)
+    pk = np.squeeze(pk)
+    inp_contrast = np.squeeze(get_contrast(t, inp[None,:]))
+    output_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(out, d) for d in delays]),
+        inp[None,:]
+    ))
+    ss_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(ss, d) for d in delays]),
+        inp[None,:]
+    ))
+    pk_conc_corr = np.max(corrcoef(
+        np.vstack([np.roll(pk, d) for d in delays]),
+        inp_contrast[None,:]
+    ))
+    
+    corr['xy'].append('response vs. concentration')
+    corr['corr'].append(output_conc_corr)
+    corr['neuron'].append('pn')
+    
+    corr['xy'].append('steady-state vs. concentration')
+    corr['corr'].append(ss_conc_corr)
+    corr['neuron'].append('pn')
+    
+    corr['xy'].append('transient vs. contrast')
+    corr['corr'].append(pk_conc_corr)
+    corr['neuron'].append('pn')
+
+# %%
+fig,ax = plt.subplots(1,1,figsize=(8,8))
+
+vp = sns.boxplot(data=corr, x="corr", y="xy", hue="neuron", width=.5)
+for patch in ax.artists:
+    r, g, b, a = patch.get_facecolor()
+    patch.set_facecolor((r, g, b, .3))
+sns.swarmplot(data=corr, x="corr", y="xy", hue="neuron", dodge=True, size=8, color='.25')
+_ = ax.set(
+    xlim=[0, 1], 
+    xlabel='Maximum Correlation', 
+    ylabel='Comparison', 
+    title='Comparison of Concentration Invariance and Contrast-Boosting Properties of OSN and PN physiology responses'
+)
---
+++ notebooks/example_circuit.py
@@ -0,0 +1,499 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Example Circuit
+# In this notebook, we demonstrate an example spatio-temporal circuit of the Antennal Lobe with the following configuration:
+#
+# 1. Pre-LN: Global Feedback Inhibition of OSN Axon-Terminal
+# 2. Post-eLN: Local Feedforward Excitation of PN
+# 3. Post-iLN: Local Feedforward Inhibition of PN
+#
+# Note that the model described here is a rate-based model, where the Frequency-Current curve
+# of the Biophysical Spike Generators (e.g. PN Axon Hillock) are represented as 
+# Poisson Spike Generators. The rate of the Poisson Processes of each BSG is represented
+# by the Frequency-Current curve of a Noisy Connor-Stevens neuron. 
+# This was done for computational efficiency, which allows the massive experimentations 
+# across parameterizations and circuit configurations in our work.
+# However, note that the circuit can be easily changed to a spiking network by changing 
+# the model describing each BSG in the circuit.
+#
+# See [[LAY20]](https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1007751#sec037) for more details on the Noisy Connor-Stevens neuron model.
+#
+# ## Requirements
+# ```
+# pandas>=1.2.3
+# numpy>=1.20
+# pycuda>=2021.1
+# matplotlib>=3.4
+# scikit-cuda>=0.5.3
+# neural
+# ```
+#
+# Note that `neural` is an in-house package that supports massively parallel evaluation of 
+# biological neural networks. It is written purely in Python and supports a natural 
+# API. This notebook, was developed using a specific pinned version of neural,
+# and can be installed from GitHub [here](https://github.com/chungheng/neural/tree/al).
+
+# %%
+import random
+import pandas as pd
+import numpy as np
+import pycuda.autoprimaryctx
+from neural.basemodel import Model
+from neural.network import Network
+from neural.network.operator import Operator, Repeat, Add
+from pycuda.elementwise import ElementwiseKernel
+import pycuda.gpuarray as garray
+import matplotlib.pyplot as plt
+
+import skcuda
+import skcuda.misc
+skcuda.misc.init()
+
+# %% [markdown]
+# ### Loading I/O
+
+# %%
+df_aff = pd.read_csv('../data/estimated_affinity.csv', index_col=0)
+data = np.load('../data/staircase.npz', allow_pickle=True)
+t = data['t']
+stim = data['pn_input'][2]
+pn = data['pn_input'][2]
+dt = t[1] - t[0]
+
+aff = df_aff.loc['acetone'].values # affinity rate of acetone
+dr = 10. # dissociation rate fixed
+N_r = len(aff) # N_r: number of receptors
+
+
+# %% [markdown]
+# ### Model Definitions
+
+# %%
+class PostLNSyn(Model):
+    Default_States = dict(
+        x1=(0.0, 0.0, 1.0), x2=(0.0, 0.0, 1.0), x3=(1.0, 0.0, 1.0), I=0.0
+    )
+    Default_Params = dict(
+        a1=1.0,
+        b1=1.0,
+        k=10.0,
+        a2=1.0,
+        b2=1.0,
+        a3=1.0,
+        b3=1.0,
+        gmax=1.0,
+    )
+
+    def ode(self, u=0.0):
+        self.d_x1 = self.a1 * self.x2 * (1.0 - self.x1) - self.b1 * self.x1- self.k * self.x1 * self.x3
+        self.d_x2 = self.a2 * u * (1.0 - self.x2) - self.b2 * self.x2
+        self.d_x3 = self.a3 * u * (1.0 - self.x3) - self.b3 * self.x3
+        self.I = self.x1 * self.gmax
+
+
+class PreLN(Operator):
+    """BlockMean +  Repeat"""
+
+    def __init__(self, operation="mean", block_size=1, **kwargs):
+        assert operation in ["mean", "sum"]
+        self.operation = operation
+        self.block_size = block_size
+        mean_output_size = int(kwargs["size"] // block_size)
+        super().__init__(**kwargs)
+        self.meaned = garray.empty(mean_output_size, dtype=self.dtype)
+
+        self._repeat_kernel = ElementwiseKernel(
+            "{0} *output, {0} *input".format(self.dtype),
+            "output[i] = input[i / {}]".format(self.block_size),
+            "Repeat",
+        )
+
+    def update(self, input):
+        if np.isscalar(input):
+            input = np.array([input])
+        _input = input.reshape(-1, self.block_size)
+        if self._backend == "cuda":
+            if self.operation == "mean":
+                skcuda.misc.mean(_input, out=self.meaned, axis=1)
+            elif self.operation == "sum":
+                skcuda.misc.sum(_input, out=self.meaned, axis=1)
+            self._repeat_kernel(self.output, self.meaned)
+        else:
+            raise NotImplementedError
+
+
+class dDNP(Model):
+    Default_States = dict(
+        x1=(0.0, 0.0, 1.0),
+    )
+    Default_Params = dict(
+        a1=10.0,
+        b1=1.0,
+        k=10.0,
+    )
+
+    def ode(self, u=0.0, l=0.0):
+        self.d_x1 = self.a1 * u * (1.0 - self.x1) - self.b1 * self.x1 - self.k * self.x1 * l
+
+
+class PoissonCSN0(Model):
+    Default_States = dict(
+        spike_rate=(0.0, 0.0, 350.0), x=(0.0, 0.0, 1.0), r=0.0, spike=0.0, cx=0.0
+    )
+    Default_Params = dict(
+        x7=7.06672200e-12,
+        x6=-4.41125703e-09,
+        x5=1.14814892e-06,
+        x4=-1.61863859e-04,
+        x3=1.34591675e-02,
+        x2=-6.79691186e-01,
+        x1=2.25636389e01,
+        x0=-1.36299580e02,
+    )
+
+    def ode(self, I=0.0):
+        self.x = 0.0
+        Ip = 1.0
+        self.d_x = Ip * self.x0
+        Ip = Ip * I
+        self.d_x += Ip * self.x1
+        Ip = Ip * I
+        self.d_x += Ip * self.x2
+        Ip = Ip * I
+        self.d_x += Ip * self.x3
+        Ip = Ip * I
+        self.d_x += Ip * self.x4
+        Ip = Ip * I
+        self.d_x += Ip * self.x5
+        Ip = Ip * I
+        self.d_x += Ip * self.x6
+        Ip = Ip * I
+        self.d_x += Ip * self.x7
+        self.r = random.uniform(0.0, 1.0)
+
+    def post(self):
+        self.spike = self.r < self.x
+        self.cx += self.x
+
+
+class PoissonCSN(Model):
+    Default_States = dict(x=0.0, y=0.0, r=0.0, spike=0.0, cx=0.0)
+    Default_Params = dict(
+        x6=2.79621009e-09,
+        x5=-9.55636291e-07,
+        x4=1.25880567e-04,
+        x3=-7.79496241e-03,
+        x2=1.94672932e-01,
+        x1=3.44246777,
+        x0=5.11085315,
+    )
+
+    def ode(self, I=0.0):
+
+        self.x = 0.0
+        Ip = 1.0
+        self.d_x = Ip * self.x0
+        Ip = Ip * I
+        self.d_x += Ip * self.x1
+        Ip = Ip * I
+        self.d_x += Ip * self.x2
+        Ip = Ip * I
+        self.d_x += Ip * self.x3
+        Ip = Ip * I
+        self.d_x += Ip * self.x4
+        Ip = Ip * I
+        self.d_x += Ip * self.x5
+        Ip = Ip * I
+        self.d_x += Ip * self.x6
+        self.y = self.d_x
+
+        self.r = random.uniform(0.0, 1.0)
+
+    def post(self):
+        self.spike = self.r < self.x
+        self.cx += self.x
+
+class PoissonCSN1(Model):
+    Default_States = dict(
+        x=(0.0, 0., 1.), 
+        r=0.0, 
+        spike=0.0, 
+        cx=0.0
+    )            
+    Default_Params = dict(
+        x8=  2.62834699e-12,
+        x7= -1.18820316e-09,
+        x6=  2.24914051e-07,
+        x5= -2.30971695e-05,
+        x4=  1.38994273e-03,
+        x3= -4.88554093e-02,
+        x2=  8.87442880e-01,
+        x1= -6.90178752e-01,
+        x0=  8.95839969e-01,
+    )
+
+    def ode(self, I=0.0):
+        self.x = 0.0
+        Ip = 1.0
+        self.d_x = Ip * self.x0
+        Ip = Ip * I
+        self.d_x += Ip * self.x1
+        Ip = Ip * I
+        self.d_x += Ip * self.x2
+        Ip = Ip * I
+        self.d_x += Ip * self.x3
+        Ip = Ip * I
+        self.d_x += Ip * self.x4
+        Ip = Ip * I
+        self.d_x += Ip * self.x5
+        Ip = Ip * I
+        self.d_x += Ip * self.x6
+        Ip = Ip * I
+        self.d_x += Ip * self.x7
+        Ip = Ip * I
+        self.d_x += Ip * self.x8
+        self.r = random.uniform(0.0, 1.0)
+
+    def post(self):
+        self.spike = self.r < self.x
+        self.cx += self.x
+
+class OTP(Model):
+    Default_States = dict(
+        v=(0.0, 0, 1e9),
+        I=0.0,
+        uh=(0.0, 0.0, 1e9),
+        duh=0.0,
+        x1=(0.0, 0.0, 1.0),
+        x2=(0.0, 0.0, 1.0),
+        x3=(0.0, 0.0, 1000.0),
+    )
+    Default_Params = dict(
+        br=1.0,
+        dr=1.0,
+        gamma=0.215,
+        b1=0.8,
+        a1=45.0,
+        a2=146.1,
+        b2=117.2,
+        a3=2.539,
+        b3=0.9096,
+        kappa=8841,
+        p=1.0,
+        c=0.06546,
+        Imax=85.0,
+    )
+
+    def ode(self, stimulus=0.0):
+        self.d_x1 = self.br * self.v * (1.0 - self.x1) - self.dr * self.x1
+        f = np.cbrt(self.x2 * self.x2) * np.cbrt(self.x3 * self.x3)
+        self.d_x2 = self.a2 * self.x1 * (1.0 - self.x2) - self.b2 * self.x2 - self.kappa * f
+        self.d_x3 = self.a3 * self.x2 - self.b3 * self.x3
+
+        self.I = self.Imax * self.x2 / (self.x2 + self.c)
+
+        self.d_uh = self.duh
+        self.d_duh = -2 * self.a1 * self.b1 * self.duh + self.a1 * self.a1 * (stimulus - self.uh)
+        self.v = self.uh + self.gamma * self.duh
+
+
+class IonSyn(Model):
+    Default_States = dict(
+        x1=(0.0, 0.0, 1.0),
+        I=0.0,
+    )
+    Default_Params = dict(
+        a1=10.0,
+        b1=1.0,
+        gmax=10.0,
+    )
+
+    def ode(self, u=0.0):
+        self.d_x1 = self.a1 * u * (1.0 - self.x1) - self.b1 * self.x1
+        self.I = self.x1 * self.gmax
+
+
+# %% [markdown]
+# Specify circuit parameterization
+
+# %%
+params = {
+    "osn_axt": {
+        "a1": 101.6,
+        "b1": 3.12,
+        "k": 1e5
+    },
+    "osn-to-preln": {
+        "a1": 1.06,
+        "b1": 15.5,
+        "gmax": 3232.6,
+    },
+    "osn-to-posteln": {
+        'a1': 2.858053089117711,
+        'b1': 1.4540529550164887,
+        'k': 323039.26691518247,
+        'a2': 1.5028414134309191,
+        'b2': 98.82274646620965,
+        'a3': 8.583048572622092,
+        'b3': 5.9266873782378555,
+        'gmax': 6.791877e+07
+    },
+    "osn-to-postiln": {
+        'a1': 12.537946628371419,
+        'b1': 3.5533912946105644,
+        'k': 850654.7752613979,
+        'a2': 64.39457099988905,
+        'b2': 14.7847156711102,
+        'a3': 4.946413745118113,
+        'b3': 23.05951238421643,
+        'gmax': 2.463306e+04
+    },
+    "osn_axt-to-pn": {
+        'a1':1.,
+        'b1':100.,
+        'gmax':1.273e5
+    },
+    "posteln-to-pn": {
+        'a1':1.,
+        'b1':100.,
+        'gmax':20.85
+    },
+    "postiln-to-pn": {
+        'a1':1.,
+        'b1':100.,
+        'gmax':89.5
+    }
+}
+
+# %% [markdown]
+# Create Network
+
+# %%
+nn = Network(solver="euler")
+inp = nn.input(num=1, name='stim')
+inp_repeat = nn.add(Repeat, num=1, rep_size=N_r, name='stim-rep')
+osn_otp = nn.add(OTP, num=N_r, br=dr*aff, dr=dr, name='OSN-OTP')
+osn_bsg = nn.add(PoissonCSN, num=N_r, name='OSN-BSG')
+
+osn_axt = nn.add(dDNP, num=N_r, **params["osn_axt"], name="OSN-Axt")
+preln_syn = nn.add(IonSyn, num=N_r, **params["osn-to-preln"], name="OSN to PreLN")
+preln_agg = nn.add(PreLN, num=N_r, operation='sum', block_size=N_r, name="Pre-LN Input")
+preln = nn.add(PoissonCSN0, num=N_r, name="Pre-LN")
+osn_axt_to_pn = nn.add(IonSyn, num=N_r, name="OSN-Axt to PN", **params['osn_axt-to-pn'])
+
+posteln_syn = nn.add(PostLNSyn, num=N_r, name="OSN to Post-eLN", **params["osn-to-posteln"])
+posteln = nn.add(PoissonCSN0, num=N_r, name="Post-eLN")
+posteln_to_pn = nn.add(IonSyn, num=N_r, name="Post-eLN to PN", **params['posteln-to-pn'])
+
+postiln_syn = nn.add(PostLNSyn, num=N_r, name="OSN to Post-iLN", **params["osn-to-postiln"])
+postiln = nn.add(PoissonCSN0, num=N_r, name="Post-iLN")
+postiln_to_pn = nn.add(IonSyn, num=N_r, name="Post-iLN to PN", **params['postiln-to-pn'])
+
+pn_inp = nn.add(Add, num=N_r, name='PN-Dendrite')
+pn_bsg = nn.add(PoissonCSN1, num=N_r, name='PN-BSG')
+
+inp_repeat(input=inp)
+osn_otp(stimulus=inp_repeat.output)
+osn_bsg(I=osn_otp.I)
+osn_axt(u=osn_bsg.x, l=preln.x)
+
+preln_syn(u=osn_axt.x1)
+preln_agg(input=preln_syn.I)
+preln(I=preln_agg.output)
+osn_axt_to_pn(u=osn_axt.x1)
+
+posteln_syn(u=osn_bsg.x)
+posteln(I=posteln_syn.I)
+posteln_to_pn(u=posteln.x)
+
+postiln_syn(u=osn_bsg.x)
+postiln(I=postiln_syn.I)
+postiln_to_pn(u=postiln.x)
+
+pn_inp(u1=osn_axt_to_pn.I, u2=posteln_to_pn.I, u3=postiln_to_pn.I)
+pn_bsg(I=pn_inp.output)
+
+nn.compile(dtype=float, backend="cuda")
+
+osn_bsg.record('x')
+posteln.record('x')
+postiln.record('x')
+posteln_syn.record('x1')
+postiln_syn.record('x1')
+
+osn_axt.record('x1')
+osn_axt_to_pn.record('I')
+preln.record('x')
+pn_bsg.record('x')
+pn_inp.record('output')
+
+# %% [markdown]
+# Load input signal to the network instance
+
+# %%
+inp_g = garray.to_gpu(np.ascontiguousarray(stim))
+inp(inp_g)
+
+# %% [markdown]
+# Run Network
+
+# %%
+nn.run(dt, verbose=True)
+
+# %% [markdown]
+# Plot Model I/O
+
+# %%
+ds = 1000 # downsample for faster plotting
+cmap = plt.cm.Greys
+fig,axes = plt.subplots(1,4,figsize=(20,4), sharex=True, sharey=True)
+cax = axes[0].pcolormesh(
+    t[::ds],
+    df_aff.columns,
+    pn_bsg.recorder.x[:,::ds]/dt,
+    shading='nearest',
+    cmap=cmap
+)
+plt.colorbar(cax, ax=axes[0], label='PSTH [Hz]')
+axes[0].set(title='PN PSTH', xlabel='Time [sec]', ylabel='Receptor Type')
+cax = axes[1].pcolormesh(
+    t[::ds],
+    df_aff.columns,
+    posteln.recorder.x[:,::ds]/dt,
+    shading='nearest',
+    cmap=cmap
+)
+plt.colorbar(cax, ax=axes[1], label='PSTH [Hz]')
+axes[1].set(title='Post-eLN PSTH', xlabel='Time [sec]', ylabel='Receptor Type')
+cax = axes[2].pcolormesh(
+    t[::ds],
+    df_aff.columns,
+    postiln.recorder.x[:,::ds]/dt,
+    shading='nearest',
+    cmap=cmap
+)
+plt.colorbar(cax, ax=axes[2], label='PSTH [Hz]')
+axes[2].set(title='Post-iLN PSTH', xlabel='Time [sec]', ylabel='Receptor Type')
+cax = axes[3].pcolormesh(
+    t[::ds],
+    df_aff.columns,
+    osn_axt_to_pn.recorder.I[:,::ds],
+    shading='nearest',
+    cmap=cmap
+)
+plt.colorbar(cax, ax=axes[3], label='Current [$\mu$ A]')
+_ = axes[3].set(title='OSN Axon-Terminal to PN Synaptic Current', xlabel='Time [sec]', ylabel='Receptor Type')
